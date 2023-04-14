736499163f0835aa9b725a42b36a3c4d1799df0e
Tim <tingyushi@foxmail.com>
2023-04-15 00:12:59 +0800

    完善SpringSecurity配置

--- src/main/java/org/eu/miyaweb/itbaimaprojectserver/config/SecurityConfig.java
+++ src/main/java/org/eu/miyaweb/itbaimaprojectserver/config/SecurityConfig.java
@@ -1,22 +1,26 @@
 package org.eu.miyaweb.itbaimaprojectserver.config;
 
+import com.alibaba.fastjson.JSONObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.eu.miyaweb.itbaimaprojectserver.entity.RestBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -26,21 +30,31 @@ public class SecurityConfig {
         http
                 .formLogin()
                 .loginProcessingUrl("/api/auth/login")
-                .successHandler(this::onAuthenticationSuccess);
-
+                .successHandler(this::onAuthenticationSuccess)
+                .failureHandler(this::onAuthenticationFailure);
         http
                 .logout()
                 .logoutUrl("/api/auth/logout");
-
         http
                 .csrf()
                 .disable();
+
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(this::onAuthenticationFailure);
 
         return http.build();
     }
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.getWriter().write("登录成功！！！");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.success("登录成功")));
     }
+
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(RestBean.failure(401, exception.getMessage())));
+    }
+
 
 }
